e7073c4c34d5c86778a94b309403184c41259b83
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2024-11-11 20:03:43 +0000

    debug

--- builder/Program.cs
+++ builder/Program.cs
@@ -5,11 +5,10 @@ var dir = new DirectoryInfo(Directory.GetCurrentDirectory());
 
 foreach (var file in dir.Parent?.GetFiles("*.html", new EnumerationOptions() { RecurseSubdirectories = true }) ?? [])
 {
-    Console.WriteLine(file.FullName);
     using var fileStrem = file.OpenRead();
     var content = BlockquoteFormatter.FormatBlockquotes(fileStrem);
-    Console.WriteLine(content);
     content = SvgFormatter.ContentFormat(content);
+    Console.WriteLine($"Portellas builder say->'{file.FullName}' success!");
 }
 
 
